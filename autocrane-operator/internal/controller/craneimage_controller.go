29a69beafdd311fc7f0c00a37adfe92394a8b49c
Jordan Moore <jordanbecketmoore@protonmail.com>
2025-04-19 16:44:20 -0400

    pass log to secretToAuthenticator

--- autocrane-operator/internal/controller/craneimage_controller.go
+++ autocrane-operator/internal/controller/craneimage_controller.go
@@ -134,7 +134,7 @@ func (r *CraneImageReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 			return result, nil
 		}
 		log.Info("Successfully fetched credentials secret for source registry.")
-		sourceAuth, err = secretToAuthenticator(&sourceRegistryCredentialsSecret, sourceRegistry)
+		sourceAuth, err = secretToAuthenticator(&sourceRegistryCredentialsSecret, sourceRegistry, log)
 		if err != nil {
 			log.Error(err, "Failed to create authenticator from credentials secret.")
 			// Update status to reflect failure
@@ -190,7 +190,7 @@ func (r *CraneImageReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 			return result, nil
 		}
 		log.Info("Successfully fetched credentials secret for destination registry.")
-		destinationAuth, err = secretToAuthenticator(&destinationRegistryCredentialsSecret, sourceRegistry)
+		destinationAuth, err = secretToAuthenticator(&destinationRegistryCredentialsSecret, sourceRegistry, log)
 		if err != nil {
 			log.Error(err, "Failed to create authenticator from credentials secret.")
 			// Update status to reflect failure
