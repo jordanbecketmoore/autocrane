b757a24a6ec01e8e93e2bc48e083da3cd911bbc7
Jordan Moore <jordanbecketmoore@protonmail.com>
2025-04-20 21:58:27 -0400

    add status and message to craneimagepolicy

--- autocrane-operator/api/v1beta1/craneimagepolicy_types.go
+++ autocrane-operator/api/v1beta1/craneimagepolicy_types.go
@@ -57,6 +57,8 @@ type TagPolicyDetails struct {
 type CraneImagePolicyStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	Status  string `json:"status,omitempty"`
+	Message string `json:"message,omitempty"`
 }
 
 // +kubebuilder:object:root=true
